cb1c665b94e26f85c189dc3bb2b49d07e38f50b4
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-09-09 18:03:49 -0300

    Initial files with appointments implementation and database connection

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
