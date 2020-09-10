0e41f39d5aa94a52ccce98cf26caf029f246aec1
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-09-10 17:01:17 -0300

    Add jsonwebtoken library. Add JWT token generation.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,7 +32,12 @@ class AuthenticateUserService {
       throw new Error('Invalid email and/or password!');
     }
 
-    return { user };
+    const token = sign({}, '35055eae8ef3292c287013b7337f9693', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
