a9f44bda7be15b2eb72e91fa2c35773daca1b05b
Daniel Carvalho <carvalho.rochadan@gmail.com>
2021-03-06 22:00:28 -0300

    Refactor hash function on CreateUserService and AuthenticateUserService to a HashProvider to apply single responsability principle and dependency injection. Refactor authentication test.

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,16 +1,25 @@
-import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUsersService from './AuthenticateUserService';
 import CreateUsersService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUsersService(fakeUsersRepository);
-    const authenticateUser = new AuthenticateUsersService(fakeUsersRepository);
+    const createUser = new CreateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -22,5 +31,6 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,18 @@
-import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import CreateUsersService from './CreateUserService';
 
 describe('CreaUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUsersService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -18,7 +25,12 @@ describe('CreaUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUsersService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
